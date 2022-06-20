0af060e02172a8167f6f209050a9cd18ec8a59c4
Robert Pal <92271019+rj-pal@users.noreply.github.com>
2022-06-20 16:43:31 -0400

    Update notebooks

--- This is Jeopardy/jeopardy.py
+++ This is Jeopardy/jeopardy.py
@@ -1,5 +1,8 @@
-# Two functions below default to column 'question' but can be used with any name for the question column in the jeopardy data base
+from datetime import datetime
+from random import randint
+from pandas import merge
 
+# Two functions below default to column 'question' but can be used with any name for the question column in the jeopardy data base
 
 def keyword_string_filter(data, words, col_name='question' ):
     """Returns a Pandas Series of boolean values for the word list filter regardless of letter case and any word string that matches"""
@@ -20,6 +23,7 @@ def keyword_filter(data, words, col_name='question'):
     
     return filtered_series
 
+
 # Function defaults to the exact match keyword filter function, but function containing any string match can be used
 
 def filtered_df(data, word_filter, filter_function=keyword_filter):
@@ -29,22 +33,153 @@ def filtered_df(data, word_filter, filter_function=keyword_filter):
     
     
 def split_df(data):
-    """Splits the data frame into Jeopardy and Double Jeopardy rounds, and Final Jeopardy and Tiebreaker rounds"""
+    """Splits the data frame into Jeopardy and Double Jeopardy rounds, and Final Jeopardy and Tiebreaker rounds, and returns new data frames"""
     regular_jeopardy_data = data.copy()[~(data.value == 'None')]
     final_jeopardy_data = data.copy()[(data.value == 'None')]
     
     return regular_jeopardy_data, final_jeopardy_data
 
-def float_value_column_df(data, col_name='value', new_col_name='new_value'):
-    """Returns the data frame with a new column of the Jeopardy dollar value column as type Float"""
-    data_copy = data.copy()
+
+def add_float_value_column(data, new_col_name='new_value'):
+    """Returns a new data frame with a new column of the Jeopardy dollar value column as type Float"""
     col_filter = lambda value: 0 if value == 'None' else float(value.lstrip('$').replace(',', ''))
-    new_col = data[col_name].apply(col_filter)
-    data_copy[new_col_name] = new_col
+    new_col = data.iloc[:, 4].apply(col_filter)
+    data[new_col_name] = new_col
     
-    return data_copy
+    
+
+def add_datetime_decades_columns(data, new_col_name='decade'):
+    """Modifies the original data frame with column 'Air Date' converted to datatime object and a integer column for the decade."""
+    
+    dt_converter = lambda time: datetime.strptime(time, '%Y-%m-%d')
+    dt_air_date = data.iloc[:, 1].apply(dt_converter)
+    data.iloc[:, 1] = dt_air_date
+    decade_filter = lambda year: int(year//10 * 10)
+    decade = data.air_date.dt.year.apply(decade_filter)
+    data[new_col_name] = decade
+
+    
+def column_data(data, word_filter, col_name='decade'):
+    filtered_data = filtered_df(data, word_filter)
+    
+    col_counts = data.groupby(col_name).show_number.count()
+
+    
+    new_data = filtered_data.groupby(col_name)['show_number'].count().reset_index()
+    percentage = lambda row: row['show_number']/col_counts[row[col_name]]*100
+    new_col_name = 'as_percentage_of_' + col_name
+    new_data[new_col_name] = new_data.apply(percentage, axis=1)
+    
+    return new_data
+    
+
+def show_number_filtered_data(data, word_filter, col_name='decade'):
+    if type(word_filter) is str:
+        word_filter = word_filter.split()
+        
+    filtered_data = filtered_df(data, word_filter)
+    full_col_counts = data.groupby(col_name).count().iloc[:,0]
+
+    
+    col_counts = filtered_data.groupby(col_name).count().iloc[:,0].rename('number_of_shows')
+    col_sum = col_counts.sum()
+    col_percentage = col_counts.apply(lambda count: round(count/col_sum, 3)).rename('proportion_of_total')
+    new_data = merge(col_counts, col_percentage, on=col_name).reset_index()
+
+
+    
+    percentage = lambda row: round(row['number_of_shows']/full_col_counts[row[col_name]]*100, 3)
+    new_col_name = 'as_percentage_of_' + col_name
+    new_data[new_col_name] = new_data.apply(percentage, axis=1)
+    
+    
+    return new_data
+    
 
 def answer_frequency(data, col_name='answer'):
     """Return a Pandas Series of the frequency of answers for any Jeopardy Data Frame"""
     
     return data[col_name].value_counts()
+
+
+
+def play_jeopardy(data, rounds=5):
+    add_float_value_column(data)
+    total_score = 0
+    number = 0
+    correct = 0
+    while number < rounds :
+        score = quiz(data)
+        total_score += score
+        if score != 0:
+            correct +=1
+        print()
+        print("Round Earnings:", score,)
+        print("Total winnings:", total_score)
+        print()
+        number +=1
+    print("Correct:", str(correct) + "/" +str(rounds))
+    print("Winnings:", total_score)
+
+          
+
+def random_question(data):
+    first = 0
+    last = data.shape[0] - 1
+    number = randint(a=first, b=last)
+    
+    return data.iloc[number, 2:8]
+
+
+def quiz(data):
+    j_round, cat, val, quest, ans, score = random_question(data)
+    print("Round:", j_round)
+    print("Category:", cat)
+    print("Value:", val)
+    print("Question:", quest)
+    print()
+    answer = input("Answer: ")
+    answer_words = answer.lower().split()
+    print()
+    response = False
+
+    if answer_words: # check if there was some input and not just 'enter' was pressed
+        
+        if '(or' in ans: # check for multiple posible answers to one question 
+            ans_list = ans.split()
+            new_ans_list = []
+            new_ans_list.append(ans_list[0].lower())
+            new_ans_list.append(ans_list[-1][:-1].lower())
+            for word in answer_words: 
+                if word in new_ans_list:
+                    reponse = True
+                    break
+        elif '&' in ans: # check for answers that require more than one response
+            ans_list = ans.split()
+            new_ans_list = []
+            for a in ans_list:
+                if a != '&':
+                    new_ans_list.append(a.lower())
+            
+            response = all(word in new_ans_list for word in answer_words)
+                    
+        else:
+            for word in answer_words:
+                if word in ans.lower():
+                    response = True
+                    break
+        
+    if response:   
+        print("Correct. The answer is", ans)
+        
+        return score
+    else:
+        print("Incorrect. The answer is", ans)
+        
+        return 0
+        
+if __name__ == "__main__":
+    from pandas import DataFrame, read_csv
+    
+    data = read_csv('jeopardy.csv')
+    play_jeopardy(data)
